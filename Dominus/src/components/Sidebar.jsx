eaf528364bae4481f8646c269a9f6909d561c918
Vinícius Veronez Miranda <viniciusvmiranda@gmail.com>
2026-05-28 03:06:39 -0300

    fix: corrige capitalização dos nomes dos arquivos

--- Dominus/src/components/Sidebar.jsx
+++ Dominus/src/components/Sidebar.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import { categoriasPrincipais, subcategorias } from '../data/cardapio'
 import './Sidebar.css'
 import { UtensilsCrossed, Package, Star, BookOpen } from 'lucide-react'
-import ModalAvaliacao from './ModalAvaliacao'
-import ModalTermos from './ModalTermos'
+import Modalavaliacao from './Modalavaliacao'
+import Modaltermos from './Modaltermos'
 
 const idiomas = [
   { codigo: 'pt', bandeira: '🇧🇷' },
